cc336fa863dadbc74dfb6d5e8ab31ec578ca1bcc
Manuel Michalke <michalke@itq.de>
2017-11-07 16:05:21 +0100

    - Buttons hinzugefügt - Spielzeitzähler hinzugefügt - Kleinere Änderungen am Design - Pause-Funktion hinzugefügt

--- app/src/main/java/com/example/michalke/app_berufsschule/SinglePlayerActivity.java
+++ app/src/main/java/com/example/michalke/app_berufsschule/SinglePlayerActivity.java
@@ -2,75 +2,347 @@ package com.example.michalke.app_berufsschule;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.provider.ContactsContract;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
+import android.os.Bundle;;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SinglePlayerActivity extends AppCompatActivity
 {
-    ImageButton buttonRight, buttonLeft, buttonUpperRight, buttonUpperLeft, buttonExit;
+    Button bStartGame;
+    ImageButton buttonRight, buttonLeft, buttonUpRight, buttonUpLeft, buttonDownRight, buttonDownLeft, buttonExit;
     ImageView stickmanMove, goal;
+    TextView tTimerWatch;
+    Handler customhandler = new Handler();
+    long startTime, timeInMilliseconds, timeSwapBuff, updateTime = 0L;
 
-    //float currentX = stickmanMove.getX();
+    Runnable updateTimerThread = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            timeInMilliseconds = SystemClock.uptimeMillis()-startTime;
+            updateTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updateTime/1000);
+            int mins = secs/60;
+            secs %= 60;
+            int miliseconds = (int) (updateTime%1000);
+            tTimerWatch.setText("" + mins + ":" + String.format("%02d", secs) + ":" + String.format("%03d", miliseconds));
+            customhandler.postDelayed(this, 0);
+        }
+    };
+
+    float currentX;
+    float currentY = 450.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_player);
 
+        bStartGame = (Button) findViewById(R.id.bStartGame);
+        tTimerWatch = (TextView) findViewById(R.id.tTimerWatch);
         buttonRight = (ImageButton) findViewById(R.id.buttonRight);
         buttonRight.setBackground(null);
+        buttonRight.setEnabled(false);
         buttonLeft = (ImageButton) findViewById(R.id.buttonLeft);
         buttonLeft.setBackground(null);
-        buttonUpperRight = (ImageButton) findViewById(R.id.buttonUpperRight);
-        buttonUpperRight.setBackground(null);
-        buttonUpperLeft = (ImageButton) findViewById(R.id.buttonUpperLeft);
-        buttonUpperLeft.setBackground(null);
+        buttonLeft.setEnabled(false);
+        buttonUpRight = (ImageButton) findViewById(R.id.buttonUpRight);
+        buttonUpRight.setBackground(null);
+        buttonUpRight.setEnabled(false);
+        buttonUpLeft = (ImageButton) findViewById(R.id.buttonUpLeft);
+        buttonUpLeft.setBackground(null);
+        buttonUpLeft.setEnabled(false);
+        buttonDownRight = (ImageButton) findViewById(R.id.buttonDownRight);
+        buttonDownRight.setBackground(null);
+        buttonDownRight.setEnabled(false);
+        buttonDownLeft = (ImageButton) findViewById(R.id.buttonDownLeft);
+        buttonDownLeft.setBackground(null);
+        buttonDownLeft.setEnabled(false);
         stickmanMove = (ImageView) findViewById(R.id.stickmanMove);
         goal = (ImageView) findViewById(R.id.goal);
         buttonExit = (ImageButton) findViewById(R.id.exit);
         buttonExit.setBackground(null);
     }
 
+    public void startGame(View view)
+    {
+        buttonRight.setEnabled(true);
+        buttonLeft.setEnabled(true);
+        buttonUpRight.setEnabled(true);
+        buttonUpLeft.setEnabled(true);
+        buttonDownRight.setEnabled(true);
+        buttonDownLeft.setEnabled(true);
+        startTime = SystemClock.uptimeMillis();
+        customhandler.postDelayed(updateTimerThread, 0);
+        bStartGame.setEnabled(false);
+    }
+
 
     public void right(View view)
     {
-        Toast.makeText(SinglePlayerActivity.this, "Button RIGHT geklickt", Toast.LENGTH_SHORT).show();
-        //currentX++;
-        //stickmanMove.setX(currentX);
-        stickmanMove.setX(+ 10.0f);
+        if (currentX <= goal.getX())
+        {
+            stickmanMove.setX(currentX += 50.0f);
+        }
+        else
+        {
+            customhandler.removeCallbacks(updateTimerThread);
+            buttonRight.setEnabled(false);
+            buttonLeft.setEnabled(false);
+            buttonUpRight.setEnabled(false);
+            buttonUpLeft.setEnabled(false);
+            buttonDownRight.setEnabled(false);
+            buttonDownLeft.setEnabled(false);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Sie haben das Level mit "+ tTimerWatch.getText() + " Sekunden geschafft");
+            alertDialogBuilder.setPositiveButton("Nächstes Level?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Das nächste Level ist noch in Arbeit!", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            alertDialogBuilder.setNegativeButton("Speichern?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Speichern ist momentan noch nicht möglich!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            alertDialog.setCancelable(false);
+        }
     }
 
-    //public void left(View view)
-    //{
-    //    stickmanMove.setY(50);
-    //}
+    public void left(View view)
+    {
+        if (currentX <= goal.getX())
+        {
+            stickmanMove.setX(currentX -= 50.0f);
+        }
+        else
+        {
+            customhandler.removeCallbacks(updateTimerThread);
+            buttonRight.setEnabled(false);
+            buttonLeft.setEnabled(false);
+            buttonUpRight.setEnabled(false);
+            buttonUpLeft.setEnabled(false);
+            buttonDownRight.setEnabled(false);
+            buttonDownLeft.setEnabled(false);
 
-    //public void upperRight(View view)
-    //{
-    //    stickmanMove.setX(currentX += 10);
-    //    stickmanMove.setY(currentY += 10);
-    //}
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Sie haben das Level mit "+ tTimerWatch.getText() + " Sekunden geschafft");
+            alertDialogBuilder.setPositiveButton("Nächstes Level?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Das nächste Level ist noch in Arbeit!", Toast.LENGTH_SHORT).show();
 
-    //public void upperLeft(View view)
-    //{
-    //    stickmanMove.setX(currentX -= 10);
-    //    stickmanMove.setY(currentY += 10);
-    //}
+                }
+            });
+            alertDialogBuilder.setNegativeButton("Speichern?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Speichern ist momentan noch nicht möglich!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            alertDialog.setCancelable(false);
+        }
+    }
 
-    //Sobald x = 535 überschritten soll das Spiel beendet sein!
+    public void upRight(View view)
+    {
+        if (currentX <= goal.getX())
+        {
+            stickmanMove.setY(currentY -= 50.0f);
+            stickmanMove.setX(currentX += 50.0f);
+        }
+        else
+        {
+            customhandler.removeCallbacks(updateTimerThread);
+            buttonRight.setEnabled(false);
+            buttonLeft.setEnabled(false);
+            buttonUpRight.setEnabled(false);
+            buttonUpLeft.setEnabled(false);
+            buttonDownRight.setEnabled(false);
+            buttonDownLeft.setEnabled(false);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Sie haben das Level mit "+ tTimerWatch.getText() + " Sekunden geschafft");
+            alertDialogBuilder.setPositiveButton("Nächstes Level?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Das nächste Level ist noch in Arbeit!", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            alertDialogBuilder.setNegativeButton("Speichern?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Speichern ist momentan noch nicht möglich!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            alertDialog.setCancelable(false);
+        }
+    }
+
+    public void upLeft(View view)
+    {
+        if (currentX <= goal.getX())
+        {
+            stickmanMove.setY(currentY -= 50.0f);
+            stickmanMove.setX(currentX -= 50.0f);
+        }
+        else
+        {
+            customhandler.removeCallbacks(updateTimerThread);
+            buttonRight.setEnabled(false);
+            buttonLeft.setEnabled(false);
+            buttonUpRight.setEnabled(false);
+            buttonUpLeft.setEnabled(false);
+            buttonDownRight.setEnabled(false);
+            buttonDownLeft.setEnabled(false);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Sie haben das Level mit "+ tTimerWatch.getText() + " Sekunden geschafft");
+            alertDialogBuilder.setPositiveButton("Nächstes Level?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Das nächste Level ist noch in Arbeit!", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            alertDialogBuilder.setNegativeButton("Speichern?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Speichern ist momentan noch nicht möglich!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            alertDialog.setCancelable(false);
+        }
+    }
+
+    public void downRight(View view)
+    {
+        if (currentX <= goal.getX())
+        {
+            stickmanMove.setY(currentY += 50.0f);
+            stickmanMove.setX(currentX += 50.0f);
+        }
+        else
+        {
+            customhandler.removeCallbacks(updateTimerThread);
+            buttonRight.setEnabled(false);
+            buttonLeft.setEnabled(false);
+            buttonUpRight.setEnabled(false);
+            buttonUpLeft.setEnabled(false);
+            buttonDownRight.setEnabled(false);
+            buttonDownLeft.setEnabled(false);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Sie haben das Level mit "+ tTimerWatch.getText() + " Sekunden geschafft");
+            alertDialogBuilder.setPositiveButton("Nächstes Level?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Das nächste Level ist noch in Arbeit!", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            alertDialogBuilder.setNegativeButton("Speichern?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Speichern ist momentan noch nicht möglich!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            alertDialog.setCancelable(false);
+        }
+    }
+
+    public void downLeft(View view)
+    {
+        if (currentX <= goal.getX())
+        {
+            stickmanMove.setY(currentY += 50.0f);
+            stickmanMove.setX(currentX -= 50.0f);
+        }
+        else
+        {
+            customhandler.removeCallbacks(updateTimerThread);
+            buttonRight.setEnabled(false);
+            buttonLeft.setEnabled(false);
+            buttonUpRight.setEnabled(false);
+            buttonUpLeft.setEnabled(false);
+            buttonDownRight.setEnabled(false);
+            buttonDownLeft.setEnabled(false);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Sie haben das Level mit "+ tTimerWatch.getText() + " Sekunden geschafft");
+            alertDialogBuilder.setPositiveButton("Nächstes Level?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Das nächste Level ist noch in Arbeit!", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            alertDialogBuilder.setNegativeButton("Speichern?", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    Toast.makeText(SinglePlayerActivity.this, "Speichern ist momentan noch nicht möglich!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            alertDialog.setCancelable(false);
+        }
+    }
 
     public void exit (View view)
     {
+        timeSwapBuff += timeInMilliseconds;
+        customhandler.removeCallbacks(updateTimerThread);
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setMessage("Sie wolen das Spiel verlassen?");
+        alertDialogBuilder.setMessage("Sie wollen das Spiel verlassen?");
         alertDialogBuilder.setPositiveButton("Verlassen!", new DialogInterface.OnClickListener()
         {
             @Override
@@ -85,10 +357,13 @@ public class SinglePlayerActivity extends AppCompatActivity
             @Override
             public void onClick(DialogInterface dialog, int which)
             {
+                startTime = SystemClock.uptimeMillis();
+                customhandler.postDelayed(updateTimerThread, 0);
                 Toast.makeText(SinglePlayerActivity.this, "Sie bleiben im Spiel und es wird weiter ausgeführt!", Toast.LENGTH_SHORT).show();
             }
         });
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
+        alertDialog.setCancelable(false);
     }
 }
